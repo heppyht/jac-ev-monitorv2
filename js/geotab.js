7ce85a2c68024eff4af78289229ef63ecd91e821
Heppy <heppy@opexcg.com>
2026-07-08 12:00:40 +0700

    Update JAC EV Monitor

--- js/geotab.js
+++ js/geotab.js
@@ -4,6 +4,17 @@ let api = null;
 
 function initializeGeotab(callback) {
 
+    // Jika bukan di MyGeotab
+    if (typeof geotab === "undefined") {
+
+        console.warn("Geotab API not available. Running in browser mode.");
+
+        document.addEventListener("DOMContentLoaded", callback);
+
+        return;
+    }
+
+    // Jika dijalankan di MyGeotab
     geotab.addin.jac_monitor_index = function (apiRef) {
 
         api = apiRef;
